37e21de41866059053a296d7dea2c020a83dca40
Praba <prabakaranpk0064@gmail.com>
2023-02-09 19:01:44 +0530

    CountDown

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -224,11 +224,37 @@
     // ****************************
     // :: 13.0 Countdown Active Code
     // ****************************
-    if ($.fn.countdown) {
-        $("#clock").countdown("2023/03/23", function (event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
-        });
-    }
+    // if ($.fn.countdown) {
+    //     $("#clock").countdown("2023/03/11", function (event) {
+    //         $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
+    //     });
+    // }
+
+    // Set the date we're counting down to
+var countDownDate = new Date("Mar 11, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  
+}, 1000);
 
     // ****************************
     // :: 13.0 Scrollup Active Code
